85c952e6453d43523274f544a498bee8c4400959
Jörg-Christian Böhme <joerg@chaosdorf.de>
2018-06-13 00:46:44 +0200

    Add mingw docker

--- create_docker.sh
+++ create_docker.sh
@@ -1,1 +1,1 @@
-docker build -t ubuntu-build:1.0 ubuntu-build && docker build -t ubuntu-build-thirdparty:1.0 ubuntu-build-thirdparty && docker build -t ubuntu-build-android:1.0 ubuntu-build-android
+docker build -t ubuntu-build:1.0 ubuntu-build && docker build -t ubuntu-build-thirdparty:1.0 ubuntu-build-thirdparty && docker build -t ubuntu-build-android:1.0 ubuntu-build-android && docker build -t ubuntu-build-mingw:1.0 ubuntu-mingw
