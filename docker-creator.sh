a1194a551bac84286b14860fc83c4d51bf2a86d5
Jörg-Christian Böhme <joerg@chaosdorf.de>
2019-01-06 21:18:53 +0100

    Add usb support for neopg docker build

--- docker-creator.sh
+++ docker-creator.sh
@@ -4,10 +4,12 @@ docker run \
 --name dev-ui \
 --rm \
 -i -t \
+--privileged \
 --cap-add=SYS_PTRACE --security-opt "seccomp=unconfined" \
 -e "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" \
 -e "DISPLAY=$DISPLAY" \
 --device=/dev/dri:/dev/dri \
+-v /dev/bus/usb:/dev/bus/usb \
 -v $HOME:/home/developer \
 -v $HOME:/home/$USER \
 -v /tmp:/tmp \
