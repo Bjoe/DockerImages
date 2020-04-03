c26204a8d38e2ac7cb96e76f9bd19f9ea0ff2970
Joerg-Christian Boehme <joerg@chaosdorf.de>
2020-04-04 00:10:45 +0200

    Add build environment

---
+++ poco-build-env-ubuntu/set-build-env.sh
@@ -0,0 +1,6 @@
+export DOCKER_COMPOSE_DIR="poco-build-env-ubuntu"
+IDE_SCRIPT=run-ide-docker.sh
+BUILD_SCRIPT=build.sh
+START_UBUNTU_IDE="ide-poco"
+START_UBUNTU_BUILD="build-poco"
+START_ANDROID_BUILD="build-poco-android"
