d459898e5149ac16ac7e8f1e981385c031bc22cc
Joerg-Christian Boehme <joerg@chaosdorf.de>
2020-10-04 11:07:19 +0200

    Update qtc sourcetrail build

--- qtc-sourcetrail-build-env-ubuntu/execute-build.sh
+++ qtc-sourcetrail-build-env-ubuntu/execute-build.sh
@@ -1,17 +1,18 @@
-QTCREATOR_VERSION=4.11/4.11.0
-QTCREATOR_DIR=/home/jboehme
-export PATH="$QTCREATOR_DIR/opt/Qt/5.14.0/gcc_64/bin:$PATH"
+
+#QTCREATOR_VERSION=4.13/4.13.0
+QTCREATOR_DIR=$HOME/opt/Qt/Tools/QtCreator
+export PATH="$QTCREATOR_DIR/opt/Qt/5.15.0/gcc_64/bin:$PATH"
 
 
 cd $HOME/Development
 
 git clone https://github.com/CoatiSoftware/qtc-sourcetrail.git
 cd qtc-sourcetrail
-if [ ! -d "qt-src" ]; then
-    curl -fsSL "http://download.qt.io/official_releases/qtcreator/$QTCREATOR_VERSION/installer_source/linux_gcc_64_rhel72/qtcreator_dev.7z" -o qt-dev.7z
-    7z x -y qt-dev.7z -o"qt-src"
-    rm qt-dev.7z
-fi
+#if [ ! -d "qt-src" ]; then
+#    curl -fsSL "http://download.qt.io/official_releases/qtcreator/$QTCREATOR_VERSION/installer_source/linux_gcc_64_rhel72/qtcreator_dev.7z" -o qt-dev.7z
+#    7z x -y qt-dev.7z -o"qt-src"
+ #   rm qt-dev.7z
+#fi
 if [ -x "$(command -v qmake-qt5)" ]; then
     export QMAKE=qmake-qt5
 elif [ -x "$(command -v qmake)" ]; then
@@ -20,6 +21,6 @@ else
     echo "Neither qmake nor qmake-qt5 found"
     /bin/bash
 fi
-$QMAKE qtc-sourcetrail.pro -r "QTC_SOURCE=qt-src" "QTC_BUILD=$QTCREATOR_DIR/opt/Qt/Tools/QtCreator/lib/qtcreator" "LIBS+=-L$QTCREATOR_DIR/opt/Qt/Tools/QtCreator/lib/qtcreator" "OUTPUT_PATH=$QTCREATOR_DIR/opt/Qt/Tools/QtCreator/lib/qtcreator/plugins/"
+$QMAKE qtc-sourcetrail.pro -r "QTC_SOURCE=$QTCREATOR_DIR/dev" "QTC_BUILD=$QTCREATOR_DIR/lib/qtcreator" "LIBS+=-L$QTCREATOR_DIR/lib/qtcreator" "OUTPUT_PATH=$QTCREATOR_DIR/lib/qtcreator/plugins/"
 make clean && make
 /bin/bash
